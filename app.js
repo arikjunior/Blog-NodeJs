5efd38721a8ea0e6418895649fe9410f4d51e0ef
arikjunior <arik.junior11@gmail.com>
2022-02-15 22:41:03 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,168 @@
+const express = require('express');
+const mysql = require('mysql');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'progate',
+  password: 'password',	
+  database: 'blog'
+});
+
+app.use(
+  session({
+    secret: 'my_secret_key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use((req, res, next) => {
+  if (req.session.userId === undefined) {
+    res.locals.username = 'Tamu';
+    res.locals.isLoggedIn = false;
+  } else {
+    res.locals.username = req.session.username;
+    res.locals.isLoggedIn = true;
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/list', (req, res) => {
+  connection.query(
+    'SELECT * FROM articles',
+    (error, results) => {
+      res.render('list.ejs', { articles: results });
+    }
+  );
+});
+
+app.get('/article/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM articles WHERE id = ?',
+    [id],
+    (error, results) => {
+      res.render('article.ejs', { article: results[0] });
+    }
+  );
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs', { errors: [] });
+});
+
+app.post('/signup', 
+  (req, res, next) => {
+    console.log('Pemeriksaan nilai input kosong');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = [];
+
+    if (username === '') {
+      errors.push('Nama Pengguna kosong');
+    }
+
+    if (email === '') {
+      errors.push('Email kosong');
+    }
+
+    if (password === '') {
+      errors.push('Kata Sandi kosong');
+    }
+
+    if (errors.length > 0) {
+      res.render('signup.ejs', { errors: errors });
+    } else {
+      next();
+    }
+  },
+  (req, res, next) => {
+    console.log('Pemeriksaan email duplikat');
+    const email = req.body.email;
+    const errors = [];
+    connection.query(
+        'SELECT * FROM users WHERE email = ?',
+        [email],
+        (error, results) => {
+          if (results.length > 0) {
+            errors.push('Failed to register user');
+            res.render('signup.ejs', { errors: errors });
+          } else {
+            next();
+          }
+        }
+      );    
+  },
+  (req, res) => {
+    console.log('Pendaftaran');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (error, hash) => {
+      connection.query(
+        'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+        [username, email, hash],
+        (error, results) => {
+          req.session.userId = results.insertId;
+          req.session.username = username;
+          res.redirect('/list');
+        }
+      );
+    });
+  }
+);
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (error, results) => {
+      if (results.length > 0) {
+        // Definisikan constant `plain`
+        const plain = req.body.password;
+        
+        // Definisikan constant `hash`
+        const hash = results[0].password;
+        
+        // Tambahkan sebuah method `compare` untuk membandingkan kata sandi
+        bcrypt.compare(plain, hash, (error, isEqual) => {
+          if (isEqual) {
+            req.session.userId = results[0].id;
+            req.session.username = results[0].username;
+            res.redirect('/list');
+          } else {
+            res.redirect('/login');
+          }
+        });
+        // Hapus code di bawah
+        // Hapus code di atas
+      } else {
+        res.redirect('/login');
+      }
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    res.redirect('/list');
+  });
+});
+
+app.listen(3000);
